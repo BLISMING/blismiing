4e58f0551e4bcde1a26db04fa3f991598a271f38
BLISMING <118293811+BLISMING@users.noreply.github.com>
2023-07-02 05:15:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    perPage: 1,
+    autoplay: true,
+    pauseOnHover: false,
+    arrows: true,
+    pagination: true,
+  }).mount();
+});
+
+// Smooth scrolling
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    const target = document.querySelector(link.getAttribute('href'));
+    const offsetTop = target.offsetTop;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+   window.addEventListener('load', function() {
+  // Simulasi waktu loading selama 7 detik
+  setTimeout(function() {
+    // Menghilangkan tampilan fake loading dan menampilkan konten website dengan animasi muncul halus
+    document.getElementById('fake-loading').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+    document.getElementById('content').classList.add('content-appear');
+  }, 3000);
+});
